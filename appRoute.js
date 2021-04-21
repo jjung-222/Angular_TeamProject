7e726b005ea3451ec9cba7cb7354e39b3e300881
jjung-222 <lyj7877@naver.com>
2021-04-21 16:38:37 +0900

    first commit

---
+++ appRoute.js
@@ -0,0 +1,12 @@
+angular.module("app")
+    .config(function($locationProvider, $routeProvider) {
+        //HTML5 모드 활성화
+        // $locationProvider.html5Mode({
+        //     enabled: true,
+        //     requireBase: true
+        //}); //이게 없으면 /#!/ 가 생김
+
+        $routeProvider
+            .when("/exam01_home", {templateUrl: "views/exam01_home.html"})            
+            .otherwise({redirectTo: "/exam01_home"});
+    });
