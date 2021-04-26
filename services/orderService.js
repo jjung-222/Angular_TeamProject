cdf9c0e8f7393bdeadc8d6a18f28f11d1ea4dad6
kju1018 <kms951018@naver.com>
2021-04-26 15:48:19 +0900

    리뷰, 오더 수정

--- services/orderService.js
+++ services/orderService.js
@@ -4,17 +4,22 @@ angular.module("app")
         const BASE_URL = "http://localhost:8080/orders";
 
         return {
-            list: function(pageNo=1, searchType, keyword) {
+            list: function(pageNo=1, searchType, keyword) {// 주문리스트 조회
                 const promise = $http.get(BASE_URL, {params: {pageNo, searchType, keyword}});
                 return promise;
             },
 
-            orderView: function(orderno) {
+            orderView: function(orderno) { // 주문서 조회
                 const promise = $http.get(BASE_URL+"/"+ orderno);
                 return promise;
             },
 
-            updateStatus: function(order) {
+            orderCount: function() { // 주문서 개수
+                const promise = $http.get(BASE_URL+"/orderTotalRows");
+                return promise;
+            },
+
+            updateStatus: function(order) { // 주문서 업데이트
                 const promise = $http.put(BASE_URL, order);
                 return promise;
             }
--- services/reviewService.js
+++ services/reviewService.js
@@ -4,27 +4,32 @@ angular.module("app")
         const BASE_URL = "http://localhost:8080/productReview";
 
         return {
-            list: function(pageNo=1, searchType, keyword) {
+            list: function(pageNo=1, searchType, keyword) { // 리뷰 리스트 조회
                 const promise = $http.get(BASE_URL, {params: {pageNo, searchType, keyword}});
                 return promise;
             },
 
-            bestReview: function() {
+            bestReview: function() { // 베스트 리뷰 조회
                 const promise = $http.get(BASE_URL + "/bsetReview");
                 return promise;
             },
 
-            readReview: function(boardno) {
+            readReview: function(boardno) { // 리뷰 조회
                 const promise = $http.get(BASE_URL+"/"+ boardno);
                 return promise;
             },
-            deleteReview: function(boardno){
+            deleteReview: function(boardno){ //리뷰 삭제
                 const promise = $http.delete(BASE_URL + "/" + boardno);
 
                 return promise;
             },
 
-            battachUrl: function(boardno) {
+            reviewCount: function() { // 리뷰 개수
+                const promise = $http.get(BASE_URL+"/reviewTotalRows");
+                return promise;
+            },
+
+            battachUrl: function(boardno) { // 사진 다운
                 return BASE_URL + "/battach/" + boardno;
             }
         }
